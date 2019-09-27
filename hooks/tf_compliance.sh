d351e1b33532e889a744dd5881ba074f72cbf588
NikolayMarusenko <37263265+NikolayMarusenko@users.noreply.github.com>
2019-09-27 16:35:21 +0300

    Update tf_compliance.sh

--- hooks/tf_compliance.sh
+++ hooks/tf_compliance.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-ls -l
+#!/usr/bin/env bash
+
+set -e
+
+export PATH=$PATH:/usr/local/bin
+
+terraform plan -out=plan.out
+terraform-compliance -f git:https://github.com/NikolayMarusenko/pre-commit.git -p plan.out
+rm plan.out 
